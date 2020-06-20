5fa44fd878878370ac526952c2cc7a2711eeee88
gabejoseph <gabriel.joseph4@gmail.com>
2020-06-20 13:49:59 -0400

    added to view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,26 @@ class User < ActiveRecord::Base
     has_many :epiphanies
     has_secure_password
 
-
+    def slug
+        username = self.username
+        slug = username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+    end
+    
+    def self.find_by_slug(slug)
+        @slug = slug
+        format_slug_beginning
+        results = self.where("username LIKE ?", @short_slug)
+        results.detect do |result|
+          result.slug === @slug
+        end
+    end
+    
+    def self.format_slug_beginning
+        slug_beginning = @slug.split("-")[0]
+        slug_beginning.prepend("%")
+        slug_beginning << "%"
+        @short_slug = slug_beginning
+    end
 
 
 
