aac8c20a41816bf6ca1137f84d37132083cd7aaa
gabejoseph <gabriel.joseph4@gmail.com>
2020-06-21 12:30:52 -0400

    added in login and signup functionality

--- app/controllers/epiphanies_controller.rb
+++ app/controllers/epiphanies_controller.rb
@@ -2,7 +2,15 @@ require 'pry'
 class EpiphaniesController < ApplicationController
 
     get '/epiphanies' do 
-        binding.pry
+        if !logged_in?
+            binding.pry
+            redirect to '/login'
+        else
+            binding.pry
+            @epiphanies = Epiphany.all
+            @user = self.current_user
+            erb :'/epiphanies/epiphanies'
+        end 
     end 
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,55 @@
 require 'pry'
 class UsersController < ApplicationController
 
-    get '/login' do  
-        erb :'/users/login'
-    end 
-
-    post '/login' do 
-
-
-        @user = User.create(name: params[:name], email: params[:email], password: params[:password])
-        @user.save
-        binding.pry
-        redirect to '/epiphanies'
-    end 
-
     get '/signup' do 
-        erb :'/users/signup'
+        if logged_in?
+            redirect to '/epiphanies'
+        else 
+            erb :'/users/signup'
+        end 
     end 
 
     post '/signup' do
-
+        params.each do |label, input|
+            if input.empty?
+                redirect to "/signup"
+            end 
+        end 
+        @user = User.create(name: params[:name], email: params[:email], password: params[:password])
+        @user.save
+        session[:user_id] = @user.id
+        redirect to "/epiphanies"
     end 
+
+    get '/login' do  
+        if logged_in? 
+            redirect to '/epiphanies'
+        else 
+            erb :'/users/login'
+        end 
+    end 
+
+    post '/login' do 
+        @user = User.find_by(name: params[:name])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect to '/epiphanies'
+        else 
+            redirect to '/login'
+        end 
+    end 
+
+    get '/epiphanies' do 
+        erb :'/epiphanies/epiphanies'
+    end 
+
+    get '/logout' do
+        if logged_in?
+          session.clear
+          redirect to '/login'
+        else
+          redirect to '/'
+        end
+    end
 
 end 
